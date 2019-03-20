b6645fd0f25462c5a3655a196fbc92c171cf5bb0
unknown <brethower.k@gmail.com>
2019-03-20 16:19:53 -0400

    add isaacMessage support

--- main.lua
+++ main.lua
@@ -15,6 +15,27 @@ end
   require("descriptions.ab+."..EIDConfig["Language"])
   --]]
   
+-----------------------------------
+-------- UTILITY FUNCTIONS --------
+-----------------------------------
+
+-- retrieves the player userdata
+function getPlayer()
+  return Isaac.GetPlayer(0)
+end
+
+-- get the position of the player in-game
+-- these coordinates are used for doing stuff in-game
+function getPlayerPosition()
+  return getPlayer().Position
+end
+
+-- get the position of the player on the screen
+-- these coordinates are used for drawing to the screen
+function getPlayerScreenPosition()
+  return Isaac.WorldToScreen(getPlayerPosition())
+end
+  
 --------------------------------
 -------- PROJECT HEADER --------
 --------------------------------
@@ -23,7 +44,7 @@ Isaac.ConsoleOutput("--- CS4100 Project Initialized ---\n")
 Isaac.ConsoleOutput("----------------------------------\n")
 Isaac.ConsoleOutput("\n")
 
-modEnabled = true
+modEnabled = false
 
 --------------------------------
 -------- DEBUG COMMANDS --------
@@ -99,16 +120,61 @@ mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, onDamage, EntityType.ENTITY_PLA
 ---------------------------------
 -------- FREQUENT CHECKS --------
 ---------------------------------
+local timer = 0
+local moveLeftAndRightEvery = 100
+local moveLeftAndRightAgent = true
+
+local isaacMessage = ""
+local isaacMessageTimer = 0
+local isaacMessageTimerInitValue = 0
+
+-- sets the message to print under the player sprite for the given number of frames (duration)
+function setIsaacMessage(message, duration)
+  isaacMessage = message
+  isaacMessageTimer = duration
+  isaacMessageTimerInitValue = duration -- used to fade the text out
+end
+
+-- prints the isaacMessage under the player sprite for isaacMessageTimer frames
+function printIsaacMessage()
+  if isaacMessageTimer > 0 then
+    local opacity = isaacMessageTimer / isaacMessageTimerInitValue
+    local screenPos = getPlayerScreenPosition()
+    Isaac.RenderText(isaacMessage, screenPos.X - string.len(isaacMessage) * 3, screenPos.Y, 1, 1, 1, opacity)
+    isaacMessageTimer = isaacMessageTimer - 1
+  end
+end
+
+-- code to be run every frame
 function onRender()
   -- enable and disable the AI mod by pressing 'R' on your keyboard
   if Input.IsActionTriggered(ButtonAction.ACTION_RESTART, 0) then
     if modEnabled then
       modEnabled = false
       Isaac.ConsoleOutput("AI Mod Disabled\n")
+      setIsaacMessage("AI Mod Disabled", 100)
     else
       modEnabled = true
+      timer = 0
       Isaac.ConsoleOutput("AI Mod Enabled\n")
+      setIsaacMessage("AI Mod Enabled", 100)
     end
+  end
+  
+  -- print the isaacMessage
+  printIsaacMessage()
+  
+  -- this agent moves left and then right every moveLeftAndRightEvery tics
+  if moveLeftAndRightAgent then
+    if timer % moveLeftAndRightEvery == 0 then
+      if moveDirection == ButtonAction.ACTION_LEFT then
+        moveDirection = ButtonAction.ACTION_RIGHT
+      else
+        moveDirection = ButtonAction.ACTION_LEFT
+      end
+      timer = 0
+    end
+    timer = timer + 1
   end
 end
 
@@ -116,3 +182,7 @@ end
 -- this event is triggered every frame, which is why we are using it to check the input
 mod:AddCallback(ModCallbacks.MC_POST_RENDER, onRender)
 
+
+-- bind the MC_POST_RENDER callback to onRender
+-- this event is triggered every kill, which is why we are using it to check the last kill
+mod:AddCallback(ModCallbacks.MC_POST_ENTITY_KILL, onKill)
