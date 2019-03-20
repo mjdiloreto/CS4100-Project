2102e64642a8ca50467f4ba5bc365e346cdb549d
unknown <brethower.k@gmail.com>
2019-03-19 23:42:33 -0400

    add shooting/moving, enable/disable mod with R

--- main.lua
+++ main.lua
@@ -1,7 +1,6 @@
-require('mobdebug').start();
-StartDebug();
-local mod = RegisterMod("AI Final", 1);
-
+require('mobdebug').start();             -- enable debugging checkpoints
+StartDebug();                            -- enable debugging
+local mod = RegisterMod("AI Final", 1);  -- register mod in game
 
 --[[ uncomment this when we need to include config
 local _, err = pcall(require, "config")
@@ -15,44 +14,105 @@ if not string.match(err, "attempt to call a nil value %(method 'ForceError'%)") 
 end
   require("descriptions.ab+."..EIDConfig["Language"])
   --]]
-Isaac.ConsoleOutput("----------------------------\n--- AI Final Initialized ---\n----------------------------\n")
--- Make Isaac invincible
-Isaac.ExecuteCommand("debug 3") 	
--- Make all enemies in room die
---Isaac.ExecuteCommand("debug 10")
+  
+--------------------------------
+-------- PROJECT HEADER --------
+--------------------------------
+Isaac.ConsoleOutput("----------------------------------\n")
+Isaac.ConsoleOutput("--- CS4100 Project Initialized ---\n")
+Isaac.ConsoleOutput("----------------------------------\n")
+Isaac.ConsoleOutput("\n")
 
-function onInputRequest(arg0, entity, inputHook, buttonAction)
-  if entity ~= nil then
-    if inputHook == InputHook.GET_ACTION_VALUE then
-      if buttonAction == ButtonAction.ACTION_RIGHT then
-        return 1.0
+modEnabled = true
+
+--------------------------------
+-------- DEBUG COMMANDS --------
+--------------------------------
+-- Define debug variables
+local makeIsaacInvincible = true
+local killAllEnemiesOnRoomStart = false
+
+function onGameStarted()
+  Isaac.ConsoleOutput("### New Game Started ###\n")
+  
+  -- Isaac takes damage but his health never decreases
+  if makeIsaacInvincible then
+    Isaac.ExecuteCommand("debug 3")
+  end
+  
+  -- Make all enemies in room die when entering the room
+  if killAllEnemiesOnRoomStart then
+    Isaac.ExecuteCommand("debug 10")
+  end
+  
+  -- print variables to console
+  Isaac.ConsoleOutput(string.format("makeIsaacInvincible = %s\n", tostring(makeIsaacInvincible)))
+  Isaac.ConsoleOutput(string.format("killAllEnemiesOnRoomStart = %s\n", tostring(killAllEnemiesOnRoomStart)))
+end
+
+-- bind the MC_POST_GAME_STARTED callback to onGameStarted
+-- this event is called when a new game is started or a game is loaded from a save state
+mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, onGameStarted)
+
+------------------------------------
+-------- PROGRAMMATIC INPUT --------
+------------------------------------
+local shootDirection = ButtonAction.ACTION_SHOOTUP
+local moveDirection = ButtonAction.ACTION_LEFT
+
+function onInputRequest(_, entity, inputHook, buttonAction)
+  if modEnabled then
+    if entity ~= nil then
+      if inputHook == InputHook.GET_ACTION_VALUE then
+        if buttonAction == moveDirection then
+          return 1.0
+        end
+        if buttonAction == shootDirection then
+          return 1.0
+        end
+        return nil
+      elseif inputHook == InputHook.IS_ACTION_PRESSED then
+        if buttonAction == shootDirection then
+          return true
+        end
+      elseif inputHook == InputHook.IS_ACTION_TRIGGERED then
+        -- do something here?
       end
-      return 0.0
+      return nil
     end
-    if inputHook == InputHook.IS_ACTION_PRESSED then
-      if buttonAction == ButtonAction.ACTION_SHOOTDOWN then
-        return true
-      end
-      
-    end
-    if inputHook == InputHook.IS_ACTION_TRIGGERED then
-      -- do thing here
-      return false
-    end
-    return false
   end
 end
 
-function onDamage(_,entity,_,_,source)
+-- bind the MC_INPUT_ACTION callback to onInputRequest
+mod:AddCallback(ModCallbacks.MC_INPUT_ACTION, onInputRequest)
+
+-------------------------------
+-------- DAMAGE EVENTS --------
+-------------------------------
+function onPlayerDamage(_,entity,_,_,source)
 	Isaac.ConsoleOutput("onDamage Triggered:\n")
-  --Isaac.GetPlayer(0):FireTear(Isaac.GetPlayer(0).Position, Vector.FromAngle(90):Resized(100), false,false,false)
-  
-  -- get the player entity
-  local player = Isaac.GetPlayer(0)
-  local fireFrom = player.Position
-  local fireDir = Vector.FromAngle(90):Resized(1)
-  player:FireTear(fireFrom, fireDir, false,false,false)
 end
+
+-- bind the MC_ENTITY_TAKE_DMG callback for the Player to onPlayerDamage
 mod:AddCallback(ModCallbacks.MC_ENTITY_TAKE_DMG, onDamage, EntityType.ENTITY_PLAYER)
 
-mod:AddCallback(ModCallbacks.MC_INPUT_ACTION , onInputRequest)
+---------------------------------
+-------- FREQUENT CHECKS --------
+---------------------------------
+function onRender()
+  -- enable and disable the AI mod by pressing 'R' on your keyboard
+  if Input.IsActionTriggered(ButtonAction.ACTION_RESTART, 0) then
+    if modEnabled then
+      modEnabled = false
+      Isaac.ConsoleOutput("AI Mod Disabled\n")
+    else
+      modEnabled = true
+      Isaac.ConsoleOutput("AI Mod Enabled\n")
+    end
+  end
+end
+
+-- bind the MC_POST_RENDER callback to onRender
+-- this event is triggered every frame, which is why we are using it to check the input
+mod:AddCallback(ModCallbacks.MC_POST_RENDER, onRender)
+
